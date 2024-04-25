1581517d03fa9db375f6deb2d82891c2395e0fce
tungtt3012 <thanhtungcis60@gmail.com>
2024-04-25 16:24:12 +0700

    Try modify app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+function One(){
+  return (<div>
+    <h2>So 1 tap 1</h2>
+    <h3>So 1 tap 2</h3>
+  </div>)
+}
+
+var Two = function (){
+  return (<div>
+    <h3>So 2 tap 1</h3>
+    <h3>So 2 tap 2</h3>
+  </div>)
+}
+var Three = () => (<div>
+    <h3>So 3 tap 1</h3>
+    <h3>So 3 tap 2</h3>
+  </div>)
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <One/>
+        <Two></Two>
+        <Three/>
         <a
           className="App-link"
           href="https://reactjs.org"
